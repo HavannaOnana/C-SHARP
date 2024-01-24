5c0b540679347b2c061a5bed7ba0c3ae13261001
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-01-24 20:14:39 +0100

    objects

--- output.cs
+++ output.cs
@@ -2,26 +2,41 @@ using System.ComponentModel.Design;
 using System.Data;
 
 
-// method for checking out
+class Human{
+ 
+  public string name;
+  public int age;
 
-static double CheckOut( params double[] prices){
-   double total= 0;
-   
-   foreach(double price in prices){
-      total=total+ price;
+  public void Eat(){
+   Console.Write($"{name} is eating");
+  }
+
+  public void sleep(){
+   Console.Write($"{name} is sleeping");
+  }
+
+}
+
+
+class Program{
+
+   static void Main(){
+
+      Human human1=new Human();
+      human1.name="Albert";
+      human1.age=33;
+
+      human1.Eat();
+      human1.sleep();
+
+
+      Human human2=new Human();
+      human2.name="Alex";
    }
 
-   return total;
-}
-
-static void Main(){
-   double total=CheckOut(3.99,2.42,12.3);
-
-   Console.WriteLine(total);
 
 }
 
-Main();
 
 
 
@@ -33,27 +48,6 @@ Main();
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-Console.ReadKey();
 
 
 
